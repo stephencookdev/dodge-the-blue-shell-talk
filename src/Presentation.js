010ca893bbc4624b04e3bf65afa80d9179bbc0e1
Stephen Cook <stephen@stephencookdev.co.uk>
2019-09-15 11:22:48 +0100

    Prepare shell of demo

--- src/Presentation.js
+++ src/Presentation.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { Presentation, PresenterModePlugin, Slide, DropDownNav } from "react-presents";
+import {
+  Presentation,
+  PresenterModePlugin,
+  Slide,
+  DropDownNav
+} from "react-presents";
 
 const slides = require("./slides/*.js");
 
 export default () => (
-  <Presentation>
+  <Presentation disableTheme>
     <PresenterModePlugin />
 
     {Object.keys(slides).map(name => (
