8714c68996bb569e64b67b600bde5b3fef528b06
Stephen Cook <stephen@stephencookdev.co.uk>
2019-09-16 23:07:33 +0100

    Add in base images instead of icons, for the start of the talk

--- src/misc/Sandbox/wrappers/index.jsx
+++ src/misc/Sandbox/wrappers/index.jsx
@@ -3,7 +3,14 @@ import styles from "../styles.scss";
 import main from "../main.html";
 
 const transformHtml = html =>
-  html.replace(/class="([^"]+)"/g, (_, c) => `class="${styles[c]}"`);
+  html.replace(
+    /class="([^"]+)"/g,
+    (_, cs) =>
+      `class="${cs
+        .split(" ")
+        .map(c => styles[c])
+        .join(" ")}"`
+  );
 
 export default () => (
   <div dangerouslySetInnerHTML={{ __html: transformHtml(main) }} />
