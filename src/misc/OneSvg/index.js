5bc824f7f545a3bb8981e76c0b3f0b4292e4196b
Stephen Cook <stephen@stephencookdev.co.uk>
2019-09-16 20:12:20 +0100

    Make more compelling OneSvg example

--- src/misc/OneSvg/index.js
+++ src/misc/OneSvg/index.js
@@ -3,30 +3,39 @@ import styles from "./styles.scss";
 
 export default () => (
   <div className={styles.oneSvg}>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5">
-      <g
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="like"
+      viewBox="0 0 5.2 5.2"
+      class="like"
+    >
+      <path
+        strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth=".3"
-        transform="translate(0 -292)"
-        style={{ stroke: "var(--primary-color)", fill: "none" }}
-      >
-        <circle cx="2.4" cy="292.92" r=".75" strokeLinecap="round" />
-        <path d="M1 297a1.4 2.7 0 0 1 1.5-2.7 1.4 2.7 0 0 1 1.4 2.7" />
-      </g>
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5">
-      <g
-        strokeLinejoin="round"
-        strokeWidth=".3"
-        transform="translate(0 -292)"
+        strokeWidth=".2"
         style={{
-          stroke: "var(--primary-color)",
-          fill: "var(--primary-color)"
+          stroke: "var(--primary-dark)",
+          fill: "none"
         }}
-      >
-        <circle cx="2.4" cy="292.92" r=".75" strokeLinecap="round" />
-        <path d="M1 297a1.4 2.7 0 0 1 1.5-2.7 1.4 2.7 0 0 1 1.4 2.7" />
-      </g>
+        d="M2.6.3L3.4 2H5L3.7 3.2 4.3 5 2.6 4M2.6.3L1.8 2H.2l1.3 1.2L.9 5l1.7-1"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="like"
+      viewBox="0 0 5.2 5.2"
+      class="like"
+      style={{
+        stroke: "var(--primary-color)",
+        fill: "var(--primary-color)"
+      }}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth=".2"
+        d="M2.6.3L3.4 2H5L3.7 3.2 4.3 5 2.6 4M2.6.3L1.8 2H.2l1.3 1.2L.9 5l1.7-1"
+      />
     </svg>
   </div>
 );
