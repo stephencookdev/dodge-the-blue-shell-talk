54c46edac14546f6d04327384aa570a44d061138
Stephen Cook <stephen@stephencookdev.co.uk>
2019-09-24 00:09:26 +0100

    Fix up the SVG animation part of the talk

--- src/Presentation.js
+++ src/Presentation.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import {
-  Presentation,
-  PresenterModePlugin,
-  Slide,
-  DropDownNav
-} from "react-presents";
+import { Presentation, Slide, DropDownNav } from "react-presents";
 import CursorHider from "./misc/CursorHider";
 
 const slides = require("./slides/*.js");
 
 export default () => (
   <Presentation disableTheme>
-    <PresenterModePlugin />
     <CursorHider />
 
     {Object.keys(slides).map(name => (
